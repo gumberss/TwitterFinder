42c3ab676df70a2920c616d70d9fef9152fe6c5a
Gustavo Marin Suppi <gustavomarinsuppi@gmail.com>
2019-06-20 09:43:39 -0300

    feat: remove old twitter files

--- src/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const SoundController = require('./controllers/TwitterController')
-
-const routes = express.Router()
-
-routes.get('/getTweet/:filter', SoundController.getTweet)
-
-module.exports = routes
